a8a840eb6cca4867f0c204b85aa83e4b85b5f63d
murasame29 <oogiriminister@gmail.com>
2023-11-16 15:16:23 +0900

    add: paset config

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -24,5 +24,9 @@ func LoadEnv(path ...string) {
 	if err := env.Parse(&config.Firebase); err != nil {
 		log.Fatalf("env load error: %v", err)
 	}
+
+	if err := env.Parse(&config.Paseto); err != nil {
+		log.Fatalf("env load error: %v", err)
+	}
 	Config = config
 }
--- cmd/config/type.go
+++ cmd/config/type.go
@@ -5,11 +5,16 @@ var Config = &config{}
 type config struct {
 	Server   Server
 	Firebase Firebase
+	Paseto   Paseto
 }
 
 type Server struct {
-	Port string `env:"SERVER_ADDR" envDefault:":8080"`
+	Port string `env:"SERVER_ADDR" envDefault:":8081"`
 }
 
 type Firebase struct {
+}
+
+type Paseto struct {
+	SecretKey string `env:"SECRET_KEY" envDefault:"abcdefghijabcdefghijabcdefghijab"`
 }
