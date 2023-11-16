8f5b09d9e20d10a669623e63157f8438dd8e405d
murasame29 <oogiriminister@gmail.com>
2023-11-16 15:17:08 +0900

    add: middleware

--- src/infrastructure/socket/router.go
+++ src/infrastructure/socket/router.go
@@ -1,7 +1,10 @@
 package socket
 
 import (
+	"github.com/Nagoya-Caravan-Hackathon-PDD/game-back/cmd/config"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/game-back/pkg/paseto"
 	"github.com/Nagoya-Caravan-Hackathon-PDD/game-back/src/application/websocket"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/game-back/src/infrastructure/middleware"
 	"github.com/labstack/echo/v4"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
@@ -10,7 +13,12 @@ func NewWsRouter() *echo.Echo {
 	e := echo.New()
 	s := &websocket.WsServer{}
 
-	e.Use(NewMiddleware(e).Auth())
+	maker, err := paseto.NewPasetoMaker(config.Config.Paseto.SecretKey)
+	if err != nil {
+		panic(err)
+	}
+
+	e.Use(middleware.NewMiddleware(e, maker).Auth)
 	e.GET("/game/:token", s.ServeWs)
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	return e
